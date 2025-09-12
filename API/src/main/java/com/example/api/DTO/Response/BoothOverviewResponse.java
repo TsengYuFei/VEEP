678e3be638b197ce9306874010aedcd39f790569
TsengYuFei <sophie99250@gmail.com>
2025-09-12 16:53:34 +0800

    修改booth overview回傳欄位
    測試沒問題

--- API/src/main/java/com/example/api/DTO/Response/BoothOverviewResponse.java
+++ API/src/main/java/com/example/api/DTO/Response/BoothOverviewResponse.java
@@ -1,25 +1,29 @@
 package com.example.api.DTO.Response;
 
 import com.example.api.Entity.Booth;
+import com.example.api.Entity.Expo;
 import lombok.Data;
 
 @Data
 public class BoothOverviewResponse {
     private Integer boothID;
     private Integer expoID;
+    private String expoName;
     private String name;
     private String avatar;
-    private String introduction;
 
     public static BoothOverviewResponse fromBooth(Booth booth) {
         BoothOverviewResponse response = new BoothOverviewResponse();
         response.setBoothID(booth.getBoothID());
         response.setName(booth.getName());
         response.setAvatar(booth.getAvatar());
-        response.setIntroduction(booth.getIntroduction());
 
         if(booth.getExpo() == null) response.setExpoID(null);
-        else response.setExpoID(booth.getExpo().getExpoID());
+        else {
+            Expo expo = booth.getExpo();
+            response.setExpoID(expo.getExpoID());
+            response.setExpoName(expo.getName());
+        }
 
         return response;
     }
