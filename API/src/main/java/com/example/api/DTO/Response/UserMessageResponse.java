c2fcbc8ca497a7f78443453883b81e838eac5312
TsengYuFei <sophie99250@gmail.com>
2025-10-29 20:10:30 +0800

    更新user message response
    新增avatar回傳欄位
    
    沒有測試

--- API/src/main/java/com/example/api/DTO/Response/UserMessageResponse.java
+++ API/src/main/java/com/example/api/DTO/Response/UserMessageResponse.java
@@ -8,7 +8,9 @@ import java.time.LocalDateTime;
 @Data
 public class UserMessageResponse {
     private String senderAccount;
+    private String senderAvatar;
     private String receiverAccount;
+    private String receiverAvatar;
     private String message;
     private LocalDateTime sendAt;
     private Boolean isRead;
@@ -16,7 +18,9 @@ public class UserMessageResponse {
     public static UserMessageResponse fromUserMessage(UserMessage userMessage) {
         UserMessageResponse response = new UserMessageResponse();
         response.setSenderAccount(userMessage.getSender().getUserAccount());
+        response.setSenderAvatar(userMessage.getSender().getAvatar());
         response.setReceiverAccount(userMessage.getReceiver().getUserAccount());
+        response.setReceiverAvatar(userMessage.getReceiver().getAvatar());
         response.setMessage(userMessage.getMessage());
         response.setSendAt(userMessage.getSendAt());
         response.setIsRead(userMessage.getIsRead());
